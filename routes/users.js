956921632b6972d6951ff8c3bef46a7b60de0a55
Kelly L <lock2846@gmail.com>
2018-04-08 18:42:28 -0600

    deleted previous info and copied boilerplate to start from scratch

--- routes/users.js
+++ routes/users.js
@@ -1,26 +1,10 @@
 'use strict';
 
-const express = require('express')
-const knex = require('../knex')
-const humps = require('humps')
-const bcrypt = require('bcrypt')
-const router = express.Router()
+const express = require('express');
 
-router.post('/', (q, s, next) => {
-  const hashedPassword = bcrypt.hashSync(q.body.password, 5)
-  knex('users')
-  .insert({
-    'first_name': q.body.firstName,
-    'last_name': q.body.lastName,
-    'email': q.body.email,
-    'hashed_password': hashedPassword
-  })
-  .returning(['id', 'first_name', 'last_name', 'email'])
-  .then((newUser) => s.json(humps.camelizeKeys(newUser[0])))
-  .catch((err) => {
-    next(err)
-  })
-})
+// eslint-disable-next-line new-cap
+const router = express.Router();
 
+// YOUR CODE HERE
 
-module.exports = router
+module.exports = router;
